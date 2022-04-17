9c6ba758f34a384e28cf6b2a830fd2759d046570
unknown <you@example.com>
2022-04-17 12:19:34 +0530

    Added: Problems Based on Exceptions

---
+++ src/com/bl/exception/Main.java
@@ -0,0 +1,7 @@
+package com.bl.exception;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Exception User Registration Problems!");
+    }
+}
